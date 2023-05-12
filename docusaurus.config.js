99d8b9fe7d768ef3ad2727795b3431786398948e
Edgard <edgardmessias@gmail.com>
2023-05-12 15:37:51 -0300

    chore: Added google gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-WBY63HR4VS",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
